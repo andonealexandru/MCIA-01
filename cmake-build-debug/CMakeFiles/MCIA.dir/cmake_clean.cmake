5b373002a6c048c902a278f9e9c752399cd0a607
andonealexandru <andonealexandru19@yahoo.com>
2022-10-10 16:43:45 +0300

    First commit - created project

---
+++ cmake-build-debug/CMakeFiles/MCIA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MCIA.dir/main.cpp.obj"
+  "MCIA.exe"
+  "MCIA.exe.manifest"
+  "MCIA.pdb"
+  "libMCIA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MCIA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
